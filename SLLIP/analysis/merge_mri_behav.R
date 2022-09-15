1834c101aa6dbdabccf4ba2830b87f708d737a0c
Tengwen Fan <tengwenfan@00-8a-76-eb-b5-ad.wlan.lsu.edu>
2022-09-15 10:52:02 -0700

    updated R script for merging language input, mri and CTOPP data

--- SLLIP/analysis/merge_mri_behav.R
+++ SLLIP/analysis/merge_mri_behav.R
@@ -1,4 +1,20 @@
+
+
+####################################
+
 mri_data=read.csv("merge_all5_parcels.csv")
-behav_data=read.csv("merge_data_0830.csv")
-merge_mri_behav=merge(behav_data,mri_data,by='subject_ID', all=TRUE, sort=TRUE,suffixes = c(".sllip",".smr"))
+nrow(mri_data)
+row_odd_even <- seq_len(nrow(mri_data)) %% 2    ###10101010101
+mri_data_row_odd <- mri_data[row_odd_even == 1, ]
+mri_data_row_even<- mri_data[row_odd_even == 0, ]
+mri_all5_parcels_ind=merge(mri_data_row_odd,mri_data_row_even,by='subject_ID',all=TRUE,sort=TRUE, suffixes = c("_zstats1_rand_large","_zstats2_struc_large"))
+
+behav_data=read.csv("merge_behav_data.csv")
+merge_mri_behav=merge(behav_data,mri_all5_parcels_ind,by='subject_ID', all=FALSE, sort=TRUE,suffixes = c("_behav","_mri"))
 write.csv(merge_mri_behav,"merge_mri_behav.csv",row.names = FALSE)
+
+
+
+
+
+
